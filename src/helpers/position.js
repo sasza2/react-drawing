5de3e876dca9cdc6399c1b72d5ff575df23c2713
sasza <sasza@onet.eu>
2020-01-12 00:56:00 +0100

    mobile touch position

--- src/helpers/position.js
+++ src/helpers/position.js
@@ -1,10 +1,28 @@
+const getMousePosition = (e) => {
+  if(!isNaN(e.clientX) && !isNaN(e.clientY)) return {
+    clientX: e.clientX,
+    clientY: e.clientY,
+  }
+
+  const { touches } = e
+  if (!touches) return null
+
+  return {
+    clientX: touches[0].clientX,
+    clientY: touches[0].clientY,
+  }
+}
+
 const position = (drawingRef, e) => {
   if (e.target !== drawingRef.current) return
 
   const rect = drawingRef.current.getBoundingClientRect();
+  const position = getMousePosition(e)
+  if (!position) return null
+
   return {
-    x: e.clientX - rect.left,
-    y: e.clientY - rect.top,
+    x: position.clientX - rect.left,
+    y: position.clientY - rect.top,
   }
 }
 
