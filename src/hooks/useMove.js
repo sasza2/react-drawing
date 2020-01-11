e59e8d15967687a1512654fbc5a90caedbbf3890
sasza <sasza@onet.eu>
2020-01-12 00:56:48 +0100

    useMove mobile touch

--- src/hooks/useMove.js
+++ src/hooks/useMove.js
@@ -7,6 +7,9 @@ const useMove = (drawingRef) => {
   const [moving, setMoving] = useState(null)  
 
   const mousedown = (e) => {
+    e.preventDefault()
+    e.stopPropagation()
+    
     const positionFromEvent = position(drawingRef, e)
     positionRef.current = positionFromEvent
     setMoving(positionFromEvent)
@@ -20,11 +23,15 @@ const useMove = (drawingRef) => {
     if (!node) return
 
     node.addEventListener('mousedown', mousedown)
+    node.addEventListener('touchstart', mousedown)
     window.addEventListener('mouseup', mouseup)
+    window.addEventListener('touchend', mouseup)
 
     return () => {
       node.removeEventListener('mousedown', mousedown)
+      node.removeEventListener('touchstart', mousedown)
       window.removeEventListener('mouseup', mouseup)
+      window.removeEventListener('touchend', mouseup)
     }
   })
 
@@ -32,7 +39,11 @@ const useMove = (drawingRef) => {
     if (!moving) return
 
     window.addEventListener('mousemove', mousemove)
-    return () => window.removeEventListener('mousemove', mousemove)
+    window.addEventListener('touchmove', mousemove)
+    return () => {
+      window.removeEventListener('mousemove', mousemove)
+      window.removeEventListener('touchmove', mousemove)
+    }
   }, [moving])
 
   return { positionRef, point: moving }
