0918a2eb583326fc20231e141075115d28270825
sasza <sasza@onet.eu>
2020-01-12 00:19:29 +0100

    refactor drawing function

--- src/helpers/draw.js
+++ src/helpers/draw.js
@@ -8,17 +8,17 @@ const draw = ({ brush, x1, y1, x2, y2 }) => {
     let yDirection = (yDiff / xDiff) || 0
     if (y1 > y2) yDirection = -yDirection
     let currY = y1
-    for(let x = x1; x !== x2; x += xDirection){
-      brush(x, currY)
-      currY += yDirection
+    const to = Math.abs(x1 - x2)
+    for(let i = 0; i < to; i++, currY += yDirection){
+      brush(x1 + xDirection * i, currY)
     }
   } else {
     let xDirection = (xDiff / yDiff) || 0
     if (x1 > x2) xDirection = -xDirection
     let currX = x1
-    for(let y = y1; y !== y2; y += yDirection){
-      brush(currX, y)
-      currX += xDirection
+    const to = Math.abs(y1 - y2)
+    for(let i = 0; i < to; i++, currX += xDirection){
+      brush(currX, y1 + yDirection * i)
     }
   }
 }
