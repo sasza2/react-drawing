06652ad85df1c349e3cc5dbb2c038adb875c5006
sasza <sasza@onet.eu>
2020-01-12 21:47:47 +0100

    update stories

--- src/Drawing.stories.js
+++ src/Drawing.stories.js
@@ -1,15 +1,24 @@
-import React from 'react'
+import React, { Fragment } from 'react'
 
-import Drawing, { brushFromSrc } from '.'
+import Drawing, { brushFromSrc, brushRect } from '.'
 
 export default { title: 'Drawing' };
 
 export const example = () => (
-  <div style={{ display: 'inline-block', border: '1px solid #000' }}>
-    <Drawing
-      brush={brushFromSrc('https://s3.amazonaws.com/iconbros/icons/icon_pngs/000/000/653/original/smile2-c.png?1512929493', { width: 30, height: 30 })}
-      height={1200}
-      width={800}
-    />
-  </div>
+  <Fragment>
+    <div style={{ display: 'inline-block', border: '1px solid #000' }}>
+      <Drawing
+        brush={brushFromSrc('https://raw.githubusercontent.com/sasza2/react-drawing/master/docs/example/smile.png', { width: 30, height: 30 })}
+        height={300}
+        width={300}
+      />
+    </div>
+    <div style={{ display: 'inline-block', border: '1px solid #000' }}>
+      <Drawing
+        brush={brushRect({ strokeStyle: 'red', width: 30, height: 20 })}
+        height={300}
+        width={300}
+      />
+    </div>
+  </Fragment>
 )
