e93b327db58543886343ea53608940241c8bdef6
sasza <sasza@onet.eu>
2020-01-15 19:47:02 +0100

    update story Drawing

--- src/Drawing.stories.js
+++ src/Drawing.stories.js
@@ -1,6 +1,6 @@
 import React, { Fragment } from 'react'
 
-import Drawing, { brushFromSrc, brushRect } from '.'
+import Drawing, { brushCustom, brushFromSrc, brushRect } from '.'
 
 export default { title: 'Drawing' };
 
@@ -22,6 +22,24 @@ export const example = () => (
     </div>
     <div style={{ display: 'inline-block', border: '1px solid #000' }}>
       <Drawing
+        height={300}
+        width={300}
+      />
+    </div>
+    <div style={{ display: 'inline-block', border: '1px solid #000' }}>
+      <Drawing
+        brush={brushCustom({
+          init: (ctx) => {
+            ctx.fillStyle = 'red'
+          },
+          draw: (ctx, x, y) => {
+            ctx.beginPath()
+            ctx.moveTo(x,y)
+            ctx.lineTo(x + 25,y + 25)
+            ctx.lineTo(x + 25, y - 25)
+            ctx.fill()
+          }
+        })}
         height={300}
         width={300}
       />
