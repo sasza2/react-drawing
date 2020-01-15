f6dbf3783acfa4dd94ab1077ab741a36deb93717
sasza <sasza@onet.eu>
2020-01-15 23:05:52 +0100

    Drawing from index

---
+++ src/Drawing.js
@@ -0,0 +1,44 @@
+import React, { forwardRef, memo, useRef } from 'react'
+import PropTypes from 'prop-types'
+
+import brushArc from './brush/arc'
+import useApi from './hooks/useApi'
+import useBrush from './hooks/useBrush'
+import useMove from './hooks/useMove'
+import useDraw from './hooks/useDraw'
+
+import './Drawing.css'
+
+const Drawing = ({
+  apiRef,
+  brush,
+  height,
+  fps,
+  width,
+}) => {
+  const canvasRef = useRef()
+  const brushRef = useBrush({ brush, canvasRef })
+  const move = useMove(canvasRef)
+  useDraw({ brushRef, canvasRef, fps, move })
+  useApi({ apiRef, brushRef, canvasRef })
+
+  return <canvas className='react-drawing' height={height} ref={canvasRef} width={width} />
+}
+
+Drawing.propTypes = {
+  apiRef: PropTypes.oneOfType([
+    PropTypes.func,
+    PropTypes.shape({ current: PropTypes.object }),
+  ]),
+  brush: PropTypes.object,
+  height: PropTypes.number.isRequired,
+  fps: PropTypes.number,
+  width: PropTypes.number.isRequired,
+}
+
+Drawing.defaultProps = {
+  brush: brushArc(),
+  fps: 30,
+}
+
+export default memo(forwardRef((props, ref) => <Drawing apiRef={ref} {...props} />))
--- src/index.js
+++ src/index.js
@@ -1,48 +1,5 @@
-import React, { forwardRef, memo, useRef } from 'react'
-import PropTypes from 'prop-types'
-
-import useApi from './hooks/useApi'
-import useBrush from './hooks/useBrush'
-import useMove from './hooks/useMove'
-import useDraw from './hooks/useDraw'
-import brushArc from './brush/arc'
-
-import './Drawing.css'
-
-const Drawing = ({
-  apiRef,
-  brush,
-  height,
-  fps,
-  width,
-}) => {
-  const canvasRef = useRef()
-  const brushRef = useBrush({ brush, canvasRef })
-  const move = useMove(canvasRef)
-  useDraw({ brushRef, canvasRef, fps, move })
-  useApi({ apiRef, brushRef, canvasRef })
-
-  return <canvas className='react-drawing' height={height} ref={canvasRef} width={width} />
-}
-
-Drawing.propTypes = {
-  apiRef: PropTypes.oneOfType([
-    PropTypes.func,
-    PropTypes.shape({ current: PropTypes.object }),
-  ]),
-  brush: PropTypes.object,
-  height: PropTypes.number.isRequired,
-  fps: PropTypes.number,
-  width: PropTypes.number.isRequired,
-}
-
-Drawing.defaultProps = {
-  brush: brushArc(),
-  fps: 30,
-}
-
-export default memo(forwardRef((props, ref) => <Drawing apiRef={ref} {...props} />))
-export { brushArc }
+export { default } from './Drawing'
+export { default as brushArc } from './brush/arc'
 export { default as brushCustom } from './brush/custom'
 export { default as brushFromSrc } from './brush/fromSrc'
 export { default as brushRect } from './brush/rect'
