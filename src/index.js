f0ceb04fae653d803b21d2ced0986ccb59501542
sasza <sasza@onet.eu>
2020-05-20 23:04:55 +0200

    export brush text

--- src/index.js
+++ src/index.js
@@ -4,3 +4,4 @@ export { default as brushCustom } from './brush/custom';
 export { default as brushFromSrc } from './brush/fromSrc';
 export { default as brushPanZoom } from './brush/panZoom';
 export { default as brushRect } from './brush/rect';
+export { default as brushText } from './brush/text';
